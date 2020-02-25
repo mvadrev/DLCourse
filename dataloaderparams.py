1b8d63a2e21bad932e980eb21ba7873cf1cab923
Srinivas Mukund <mvadrev@gmail.com>
2020-02-25 16:03:00 +0530

    Create dataloaderparams.py

---
+++ dataloaderparams.py
@@ -0,0 +1,14 @@
+# print(type(train_data))
+# print(len(train_data))
+# print(train_data.classes)
+# print(train_data.class_to_idx)
+# print(train_data.imgs[:-10])
+# print(train_data[-1][1])
+# Visualize images
+# sample_image = train_data[200][0].numpy().transpose(1, 2, 0)
+# mean = np.array([0.485, 0.456, 0.406])
+# std = np.array([0.229, 0.224, 0.225])
+# inp = std*sample_image+mean
+# inp = np.clip(inp, 0, 1)
+# plt.imshow(inp)
+# plt.show()
